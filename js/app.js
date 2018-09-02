2cfebd186335fe20bc8a8bc1a8bd79bf58d78368
Kevin Rollins <kevin.rollins71@googlemail.com>
2018-09-02 20:10:30 +0100

    removed enemy render funcion on the prototype as changed to ES6 class

--- js/app.js
+++ js/app.js
@@ -29,10 +29,6 @@ class Enemy {
     }
 }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
 
 // Now write your own player class
 // This class requires an update(), render() and
